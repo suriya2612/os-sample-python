fa3ac8bbbbfe49d0c1ff65c9d64f4bfb0d9403fe
Suriya U-ruekolan <suriya.uruekolan@gmail.com>
2018-02-23 14:17:15 +0700

    remove fucking

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello Fucking World!!!"
+    return "Hello World!!!"
 
 if __name__ == "__main__":
     application.run()
